ec5e0eacc8434b04f4ce95ba3c017ee048c91423
Radomír Bosák <radomir.bosak@gmail.com>
2019-01-31 14:49:44 +0100

    Move table processing to separate module

--- addb/list_db.py
+++ addb/list_db.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import subprocess
-
 from .cache import load_cache
+from .table import _show_table
 
 
 def list_db(args):
@@ -42,36 +41,3 @@ def list_db(args):
         table.append(row)
 
     _show_table(table, headers=headers)
-
-
-def _show_table(table, headers=None):
-    """
-    Print a 2d matrix of strings as a nice table (with header bars)
-
-    Args:
-        table: 2d matrix (list of lists of strings) of input data
-        headers: Header row
-    """
-    col_lens = [max(len(cell) for cell in col) for col in zip(*table)]
-
-    if headers:
-        col_lens = [max(collen, len(title)) for collen, title in zip(col_lens, headers)]
-        for collen, title in zip(col_lens, headers):
-            print(title.ljust(collen), end='  ')
-        print('')
-
-    # header line
-    for collen in col_lens:
-        print('=' * collen, end='  ')
-    print('')
-
-    # table data
-    for row in table:
-        for cell, maxlen in zip(row, col_lens):
-            print(cell.ljust(maxlen), end='  ')
-        print('')
-
-
-def _get_terminal_size():
-    rows, columns = subprocess.check_output(['stty', 'size']).split()
-    return rows, columns
